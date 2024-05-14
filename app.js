f4de5f0e347f2905c5caa4a2ad935e771f05e99d
Dostonbek <dostonbek200gmail.com>
2024-05-14 21:24:39 +0500

    end

--- app.js
+++ app.js
@@ -1,38 +1,77 @@
-// import product from "./data.js";
-// product.forEach((product) => {
-//     console.log(product.price)
-//     if (product.price <=100)
-//    cheepProduct 
-// });
+// N(1)
+// let numbers = [3,4,5,6,];
+// let number = numbers.reverse();
+// console.log(numbers);
 
 
-// const students = [
-//     { name: "Quincy", percent: 96 },
-//     { name: "Jason", percent: 84 },
-//     { name: "Alexis", percent: 100 },
-//     { name: "Sam", percent: 65 },
-//     { name: "Katie", percent: 90 },
-//     { name: "Anna", percent: 75 },
-//   ];
-  
-// function  getNamesByGrade(students, grade){
-// if
+// N(2)
+
+// let number1 = 10;
+// let number2 = 10;
+
+// if (number1 === 10 && number2 === 10) {
+//     console.log("number1 va number2 teng 10 ga");
+// } else {
+//     console.log("number1 va number2 teng emas 10 ga");
 // }
-function gratNamesByGrade(students,grade){
-  return students.map((students) =>{
-    if (students.percent > 85){
-      return { name: students.name, percent: students.percent, grade: 5};
-    } else if (students.percent <= 85 && students.percent > 70){
-      return { name: students.name, percent: students.percent, grade: 4}; 
-    } else{
-        return { name: students.name, percent: students.percent, grade: 3};
-    }
-  }).filter ((student) => {
-    return student.grade == grade;
-  }).map ((student) =>{
-    return student.name;
-  });
-}
-const result = gratNamesByGrade( students , 5);
-console.log(result);
+
+// N(3)
+
+// function createArray(n, arr = []) {
+//     if (n <= 0) {
+//       return arr;
+//     } else {
+//       arr.unshift(n);
+//       return createArray(n - 1, arr);
+//     }
+//   }
+
+//   // Misol uchun, n=5
+//   const n = 5;
+//   const resultArray = createArray(n);
+//   console.log(resultArray); // [1, 2, 3, 4, 5]
+
+// N(4)
+
+// const numbrs = [10, 6, 25, 30, 41, 15]; 
+
+// const result = numbrs.reduce((acc, curr) => {
+//   if (curr % 2 === 0) {
+//     acc[1] += curr; 
+//   } else {
+//     acc[0] += curr; 
+//   }
+//   return acc;
+// }, [0, 0]);
+
+// console.log(result);
+
+
+
+// N(9)
+
+// function ochiring(string) {
+//     if (string.endsWith('0')) {
+//       return string.slice(0, -1);
+//     }
+//     return string;
+//   }
+
+//   
+//   console.log(ochiring("12340")); 
+//   console.log(ochiring("56789")); 
+
+
+N(10)
+
+function engKichikSon(array) {
+    let min = Math.min(...array);
+    return min ** 4 + "";
+  }
+  
+  
+  let numbers = [3, 7, 2, 9, 5];
+  console.log(engKichikSon(numbers));
+
+
 
